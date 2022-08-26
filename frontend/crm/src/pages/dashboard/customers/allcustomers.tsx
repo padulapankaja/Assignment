9fb68d9fb50b9fac59451e237aaba4e36bde5ac9
padulapankaja <padulaguruge@gmail.com>
2022-08-26 13:31:03 +0530

    added creation date for customers

--- frontend/crm/src/pages/dashboard/customers/allcustomers.tsx
+++ frontend/crm/src/pages/dashboard/customers/allcustomers.tsx
@@ -20,6 +20,8 @@ import {
   getAllCustomers,
   updateCustomerStatus,
 } from "../../../controllers/customer.controller";
+import  dayjs from 'dayjs'
+
 interface DataType {
   key: React.Key;
   _id: string;
@@ -246,6 +248,20 @@ const AllCustomer: React.FC = () => {
       render: (_, record) => (
         <Space size="middle" key={record._id}>
           {tagSelector(record.status)}
+        </Space>
+      ),
+    },
+    {
+      title: "Create at ",
+      dataIndex: "created_at",
+      key: "created_at",
+      width: "20%",
+      ...getColumnSearchProps("created_at"),
+      sorter: (a, b) => a.status.length - b.status.length,
+      sortDirections: ["descend", "ascend"],
+      render: (_, record) => (
+        <Space size="middle" key={record._id}>
+          {dayjs(record.created_at).format('DD/MM/YYYY hh:mm:a')}
         </Space>
       ),
     },
